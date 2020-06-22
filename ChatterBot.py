32e7598c3960a89ea5427dce9f3327a389ce59d8
Kristan Panton <kristan.panton@gmail.com>
2020-06-22 10:02:34 -0400

    Tidied up code, added some new repsonses

--- ChatterBot.py
+++ ChatterBot.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter.ttk import Style, Button, Frame
+
 from nltk.chat.util import Chat, reflections
+import nltk.data
 
 
 # Took some responses from nltk.chat.eliza
@@ -10,18 +12,20 @@ class ChatterBot(Frame):
         super().__init__(master)
 
         # ~~~~~~~~~~< Appearance >~~~~~~~~~~
-        self.messagesbg = "#171A1F"
-        self.messagesfg = "#EEEEEF"
+        # self.chat_fieldbg = "#171A1F"
+        self.chat_fieldbg = "#0F0F0F"
+        self.chat_fieldfg = "#EEEEEF"
+        # self.input_fieldbg = "#1B1E23"
         self.input_fieldbg = "#1B1E23"
         self.send_btnbg = "#EEEEEF"
-        self.send_btnfg = "#171A1F"
-        self.fgcolor = "#EEEEEF"
-        self.bgcolor = "#3D4146"
+        self.send_btnfg = "#1B1E23"
+        self.fgcolor = "#3498DB"
+        self.bgcolor = "#1B1E23"
         self.bgsecondary = "#3498DB"
         self.font = "calibri"
 
         # ~~~~~~~~~~< Keybindings >~~~~~~~~~~
-        master.bind('<Return>', self.enter_keypress)
+        master.bind('<Return>', self.chat)
 
         # ~~~~~~~~~~< Resizing >~~~~~~~~~~
         master.columnconfigure(0, weight=1)
@@ -29,6 +33,7 @@ class ChatterBot(Frame):
 
         master.geometry("800x500")
 
+        # ~~~~~~~~~~< Menu >~~~~~~~~~~
         self.menubar = Menu(master)
         master.config(menu=self.menubar)
         self.options_menu = Menu(self.menubar, tearoff=0)
@@ -39,69 +44,75 @@ class ChatterBot(Frame):
 
         self.menubar.add_cascade(label="Options", menu=self.options_menu)
 
-        self.messages = Text(master,
-                             background=self.messagesbg,
-                             foreground=self.messagesfg,
-                             selectbackground=self.messagesfg,
-                             selectforeground=self.messagesbg,
-                             font=(self.font, 14),
-                             borderwidth=0,
-                             padx=5, pady=5)
-        self.messages.grid(row=0, column=0, stick="nsew")
-        self.messages.columnconfigure(0, weight=3)
-        self.messages.rowconfigure(0, weight=3)
+        # ~~~~~~~~~~< Chat Field >~~~~~~~~~~
+        self.chat_field = Text(master,
+                               background=self.chat_fieldbg,
+                               foreground=self.chat_fieldfg,
+                               selectbackground=self.chat_fieldfg,
+                               selectforeground=self.chat_fieldbg,
+                               font=(self.font, 14),
+                               borderwidth=1,
+                               relief="solid",
+                               padx=5, pady=5)
+        self.chat_field.grid(row=0, column=0, stick="nsew")
+        self.chat_field.columnconfigure(0, weight=3)
+        self.chat_field.rowconfigure(0, weight=3)
 
-        self.scrollbar = Scrollbar(master, command=self.messages.yview, borderwidth=0)
+        self.greeting = "Hi! My name is ChatterBot, what's yours?"
+
+        # ~~~~~~~~~~< Scrollbar >~~~~~~~~~~
+        self.scrollbar = Scrollbar(master, command=self.chat_field.yview)
         self.scrollbar.grid(row=0, column=1, sticky="nsew")
         self.scrollbar.columnconfigure(1, weight=1)
 
-        # Enables scrollbar to scroll messages
-        self.messages.configure(yscrollcommand=self.scrollbar.set)
+        # Enables scrollbar to scroll chat_field
+        self.chat_field.configure(yscrollcommand=self.scrollbar.set)
+
+        # ~~~~~~~~~~< User Input >~~~~~~~~~~
+        self.user_input = str()
 
         self.input_field = Entry(master,
                                  background=self.input_fieldbg, foreground=self.fgcolor,
                                  insertbackground=self.fgcolor,
                                  selectbackground=self.fgcolor, selectforeground=self.input_fieldbg,
                                  justify=RIGHT, font=(self.font, 25),
-                                 borderwidth=0)
+                                 borderwidth=1,
+                                 relief="solid")
         self.input_field.grid(row=1, column=0, sticky="nesw")
         self.input_field.columnconfigure(1, weight=3)
 
-        self.send_btn = Button(master, text="↵", style="TButton", command=self.enter_keypress)
+        # ~~~~~~~~~~< Button >~~~~~~~~~~
+        self.send_btn = Button(master, text="↵", style="TButton", command=self.chat)
         self.send_btn.grid(row=1, column=1, sticky="nsew")
         self.send_btn.columnconfigure(0, minsize=5)
 
-        # master.config(menu=self.menu_bar)
-        self.master = master
-
-        self.user_input = str()
-
         self.btn_style = Style()
-        self.input_field_style = Style()
 
         self.btn_style.theme_use("clam")
         self.btn_style.configure("TButton",
                                  font=(self.font, 20, "bold"),
                                  foreground=self.send_btnfg, background=self.send_btnbg,
                                  relief="flat",
+                                 borderwidth=0,
                                  width=0,
                                  padding=(5, 0))
+
         self.btn_style.map("TButton",
                            foreground=[("pressed", self.send_btnbg), ("active", self.send_btnbg)],
-                           background=[("pressed", "!disabled", self.bgsecondary), ("active", self.bgcolor)])
+                           background=[("pressed", "!disabled", self.bgcolor), ("active", self.bgsecondary)])
 
-        self.print_message("ChatterBot", "Hi! My name is ChatterBot, what is yours?", "#43B581")
-        self.messages.configure(state=DISABLED)
-
+        self.print_message("ChatterBot", self.greeting, "#43B581")
+        self.chat_field.configure(state=DISABLED)
         self.input_field.focus_set()
 
-        # ~~~~~~~~~~~~< NLTK >~~~~~~~~~~~~~~~~
+        # ~~~~~~~~~~~~< NLTK - Input/Response pairs >~~~~~~~~~~~~~~~~
         self.pairs = [
             (r"(Hi|Hello|Hey)(.*)", [
                 "Hello!",
                 "Hi there!",
                 "Hey!",
-                "Greetings."
+                "Greetings.",
+                "Hi. How are you doing today?"
             ]),
             (r"My name is (\w+( +\w+)*)", [
                 "Hi %1.",
@@ -177,6 +188,16 @@ class ChatterBot(Frame):
                 "I am operating at peak efficiency!",
                 "Do you really care?",
             ]),
+            (r"I (don't|do|do not) (\w+( +\w+)*) you", [
+                "Why %1 you %2 me?",
+                "What about me makes you %2 me?",
+                "I %2 you too."
+            ]),
+            (r"(It is|I'ts|They're|They are|We're|We are) (\w+( +\w+)*)", [
+                "How can you be sure that %1 %2?",
+                "Have you considered the possibility that %1 not %2?",
+                "%1 %2?"
+            ]),
             (r"(I'm|I am) (\w+( +\w+)*)", [
                 "Why are you %2?",
                 "How are you %2?",
@@ -205,14 +226,14 @@ class ChatterBot(Frame):
                 "Are you sure about that?",
                 "Maybe the opposite is true.",
             ]),
-            (r"How (are you feeling|do you feel)(.*)", [
+            (r"How (are you feeling|do you feel)", [
                 "I do not feel. I am a robot.",
                 "No no no. How are you feeling?",
                 "I'd much rather hear about you."
             ]),
             (r"You (\w+( +\w+)*)", [
                 "We should be discussing you, not me.",
-                "Why do you say that about me?",
+                "Why did you say that about me?",
                 "That may be true, but what about you?",
                 "Perhaps you %1.",
             ]),
@@ -250,48 +271,63 @@ class ChatterBot(Frame):
             (r"(.*)", [
                 "Is that even English?"
             ])
-
         ]
-        # reflections["are"] = "am"
+
         self.chat = Chat(self.pairs, reflections)
 
+    """Prints chatbot response"""
+
     def bot_response(self):
-        response = self.chat.respond(self.user_input).capitalize()
+        response = self.chat.respond(self.user_input)
+        sent_tokenizer = nltk.data.load("tokenizers/punkt/english.pickle")
+        sentences = sent_tokenizer.tokenize(response)
+        sent_capitalized = [(sent.split(" ")[0].capitalize(), sent.split(" ")[1:]) for sent in sentences]
+        sentences = [sent[0] + " " + " ".join(sent[1]) if
+                     len(sent[1]) != 0 else sent[0] for sent in sent_capitalized]
+        response = " ".join(sentences)
         self.print_message("ChatterBot", response, "#43B581")
+
+    """Helper function to print messages to chat_field"""
 
     def print_message(self, sender, text, color=None):
         if color is None:
             color = self.fgcolor
         tag_name = "color-" + color
-        self.messages.tag_configure(tag_name, foreground=color)
-        self.messages.insert(END, sender + ": ", tag_name)
+        self.chat_field.tag_configure(tag_name, foreground=color)
+        self.chat_field.insert(END, sender + ": ", tag_name)
         # May decide to have different colors for sender and message text
-        self.messages.insert(END, text + "\n", tag_name)
-        self.messages.see(END)
+        self.chat_field.insert(END, text + "\n", tag_name)
+        self.chat_field.see(END)
 
-    def enter_keypress(self, event=None):
+    """Does basic input validation and prints user input and bot response"""
+
+    def chat(self, event=None):
         self.user_input = self.input_field.get()
-        self.messages.configure(state=NORMAL)
+        self.chat_field.configure(state=NORMAL)
 
         if self.user_input == "" or all(c == " " for c in self.user_input):
-            self.messages.configure(state=DISABLED)
+            self.chat_field.configure(state=DISABLED)
             self.input_field.delete(0, END)
             return
 
         self.print_message("You", self.user_input, "#3498DB")
         self.bot_response()
-        self.messages.configure(state=DISABLED)
+        self.chat_field.configure(state=DISABLED)
         self.input_field.delete(0, END)
+
+    """Saves chat to chat.txt file"""
 
     def save_chat(self):
         with open("chat.txt", "w") as f:
-            f.write(self.messages.get("1.0", "end-1c"))
+            f.write(self.chat_field.get("1.0", "end-1c"))
+
+    """Clears the chat messaging window"""
 
     def clear_chat(self):
-        self.messages.configure(state=NORMAL)
-        self.messages.delete("1.0", END)
-        self.print_message("ChatterBot", "Hi! My name is ChatterBot, what is yours?", "#43B581")
-        self.messages.configure(state=DISABLED)
+        self.chat_field.configure(state=NORMAL)
+        self.chat_field.delete("1.0", END)
+        self.print_message("ChatterBot", self.greeting, "#43B581")
+        self.chat_field.configure(state=DISABLED)
 
 
 root = Tk()
